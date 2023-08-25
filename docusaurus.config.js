6e602dcb72e0c29aa8ac8b6215b6f7b1e310f0ff
mamoraru <maxim.moraru@univ-reims.fr>
2023-08-25 09:32:44 +0200

    change deployment branch to default

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'MesoNET', // Usually your GitHub org/user name.
   projectName: 'user-documentation', // Usually your repo name.
-  deploymentBranch: 'main',
+  //deploymentBranch: 'main', // default is gh-pages
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
